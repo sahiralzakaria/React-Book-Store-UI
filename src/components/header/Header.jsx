fde45c90d5561f1eca2a0aed71d13c3389769baa
sahir zakaria <sahir.alzakaria@gmail.com>
2025-12-11 00:51:36 +0300

    add assets

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Header = () => {
+    return (
+        <header className="header">
+            <div className="header-top">
+                <div className="header-top-menu">
+                    <i class="bi bi-list"></i>
+                </div>
+            </div>
+        </header>
+    )
+}
