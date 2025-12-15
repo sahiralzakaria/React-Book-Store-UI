c161f3884bfe002cc328c7d149d773e18d5a7f7f
sahir zakaria <sahir.alzakaria@gmail.com>
2025-12-15 14:03:34 +0300

    add styles and handleClick function to BookSlider arrows

--- frontend/src/components/book-slider/BookSlider.jsx
+++ frontend/src/components/book-slider/BookSlider.jsx
@@ -1,10 +1,24 @@
+import { useState } from 'react';
 import './bookSlider.css'
 
 const BookSlider = ({ data }) => {
+    const [slideIndex, setSlideIndex] = useState(0);
+
+    // Handle Click
+    const handleClick = (direction) => {
+        if (direction === "left") {
+            setSlideIndex(slideIndex - 1);
+        } else {
+            setSlideIndex(slideIndex + 1);
+        }
+    };
+
     return (
         <div className="book-slider-container">
-            <i className="bi bi-chevron-left book-slider-arrow-left"></i>
-            <div className="book-slider-wrapper" >
+            {slideIndex > 0 && <i onClick={() => { handleClick('left') }} className="bi bi-chevron-left book-slider-arrow-left"></i>}
+            <div
+                style={{ transform: `translateX(${slideIndex * -340}px)` }}
+                className="book-slider-wrapper" >
                 {data.map((item) => (
                     <div key={item.id} className="book-slide-item">
                         <img
@@ -13,6 +27,7 @@ const BookSlider = ({ data }) => {
                             className="book-slide-item-img"
                         />
                         <h3 className="book-slide-item-title">{item.title}</h3>
+                        <div className='rating'>Rating</div>
                         <div className="book-slider-item-price">${item.price}</div>
                         <div className="book-slider-icons-wrapper">
                             <i className="bi bi-eye-fill"></i>
@@ -21,7 +36,7 @@ const BookSlider = ({ data }) => {
                     </div>
                 ))}
             </div>
-            <i className="bi bi-chevron-right book-slider-arrow-right"></i>
+            {slideIndex < data.length - 1 && <i onClick={() => { handleClick('right') }} className="bi bi-chevron-right book-slider-arrow-right"></i>}
 
         </div>
     )
