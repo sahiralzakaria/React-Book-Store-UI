0a88b72f3789d0d7efa098b77b4e8ba8e0218a5c
sahir zakaria <sahir.alzakaria@gmail.com>
2025-12-20 15:41:34 +0300

    create search state of authors

--- frontend/src/pages/authors/Authors.jsx
+++ frontend/src/pages/authors/Authors.jsx
@@ -1,26 +1,32 @@
 import './author.css';
 import { authors } from '../../data/authors'
+import { useState } from 'react';
 
 const Authors = () => {
+    const [search, setSearch] = useState('');
+
     return (
         <section className="authors">
             <div className="authors-search-wrapper">
                 <input
                     type="search"
                     placeholder="Search in authors"
+                    value={search}
+                    onChange={e => setSearch(e.target.value)}
                 />
             </div>
             <div className="authors-wrapper">
-                {authors.map((author) => (
-                    <div key={author.id} className="author">
-                        <img
-                            src={author.image}
-                            alt={author.name}
-                            className="author-img"
-                        />
-                        <h2 className="author-name">{author.name}</h2>
-                    </div>
-                ))}
+                {authors.filter(a => a.name.toLowerCase().includes(search))
+                    .map((author) => (
+                        <div key={author.id} className="author">
+                            <img
+                                src={author.image}
+                                alt={author.name}
+                                className="author-img"
+                            />
+                            <h2 className="author-name">{author.name}</h2>
+                        </div>
+                    ))}
             </div>
         </section>
     )
