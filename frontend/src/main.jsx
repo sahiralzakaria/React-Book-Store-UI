b18d007c7528b1b144a3dbd7ae64efe045bfd9f1
sahir zakaria <sahir.alzakaria@gmail.com>
2025-12-20 23:29:22 +0300

    add toastify and create validation

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import CartProvider from './context/CartProvider.jsx'
+import 'react-toastify/dist/ReactToastify.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
--- frontend/src/pages/forms/Login.jsx
+++ frontend/src/pages/forms/Login.jsx
@@ -1,17 +1,65 @@
 import { Link } from 'react-router-dom';
 import './form.css';
+import { useState } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
 
 const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const formSubmitHandler = (e) => {
+        e.preventDefault();
+
+        if (!email.trim() || !password.trim()) {
+            toast.error("Please fill in all fields");
+            return;
+        }
+
+        if (password.length < 6) {
+            toast.error("Password must be at least 6 characters");
+            return;
+        }
+
+        // Success example
+        toast.success("Logged in successfully!");
+    };
     return (
         <div className="form-wrapper">
+            <ToastContainer
+                position="top-right"
+                autoClose={3000}
+                hideProgressBar={false}
+                newestOnTop
+                closeOnClick
+                pauseOnHover
+                draggable
+                theme="colored"
+            />
+
             <h1 className="form-title">Login to your account</h1>
-            <form className="form">
-                <input type="email" placeholder="Email" />
-                <input type="password" placeholder="Password" />
-                <button className="form-btn" type="submit">Login</button>
+
+            <form onSubmit={formSubmitHandler} className="form">
+                <input
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    type="email"
+                    placeholder="Email"
+                />
+
+                <input
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    type="password"
+                    placeholder="Password"
+                />
+
+                <button className="form-btn" type="submit">
+                    Login
+                </button>
             </form>
+
             <div className="form-footer">
-                Dont't have an account ?{" "}
+                Don't have an account?{" "}
                 <Link to="/register" className="forms-link">
                     Register
                 </Link>
