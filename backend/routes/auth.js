f883c8e668af2fc71eddb5ef9488aaa902b0f3e3
sahir zakaria <sahir.alzakaria@gmail.com>
2025-12-29 15:33:45 +0300

    Login Use

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -40,4 +40,36 @@ router.post("/register", asyncHandler(async (req, res) => {
 
 }))
 
+
+/**
+ * @desc Login User
+ * @route /api/auth/login
+ * @method POST
+ * @access public
+*/
+
+router.post("/login", asyncHandler(async (req, res) => {
+    const { error } = validateLoginUser(req.body);
+    if (error) {
+        return res.status(400).json({ message: error.details[0].message })
+    }
+
+    let user = await User.findOne({ email: req.body.email });
+    if (!user) {
+        return res.status(400).json({ message: "invalid email or password" });
+    }
+
+    const isPasswordMatch = await bcrypt.compare(req.body.password, user.password);
+    if (!isPasswordMatch) {
+        return res.status(400).json({ message: "invalid email or password" });
+    }
+
+
+    const token = null;
+
+    const { password, ...other } = user._doc;
+    res.status(200).json({ ...other, token });
+
+}))
+
 module.exports = router;
