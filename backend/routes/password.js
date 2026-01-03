19a775cb1a6c9144e3de1eae9bacc88ab00af39b
sahir zakaria <sahir.alzakaria@gmail.com>
2026-01-03 22:52:40 +0300

    add views files to reset password and add route

---
+++ backend/routes/password.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const { getForgotPasswordView } = require('../controllers/passwordController');
+const router = express.Router();
+
+// /password/forgot-password
+router
+    .route("/forgot-password")
+    .get(getForgotPasswordView)
+
+
+
+
+module.exports = router;
